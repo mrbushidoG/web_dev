316036ccf7a880e0794a3ad198e9d7bd5c41092d
Abdel Magid Ahmed <mrbushido90@gmail.com>
2024-12-28 00:08:38 +0400

    Deleted duplicated Files

--- web_dev/web_dev/jon_duckett/JAVASCRIPT&JQUERY/javascript_jquery/ch03/js/basic-function.js
+++
@@ -1,7 +0,0 @@
-var msg = 'Sign up to receive our newsletter for 10% off';
-function updateMessage(){
-    var elMessage = document.getElementById('message');
-    elMessage.textContent = msg;
-}
-
-updateMessage();
